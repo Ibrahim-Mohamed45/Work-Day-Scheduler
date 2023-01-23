f853fe43d67c67000b18c991773dd933b7028e09
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-01-23 18:51:33 +0000

    Add colour code to change text area background colour

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,50 @@ var now = dayjs().format("D MMMM YYYY");
 
 $("#currentDay").text(dayName + ' ' + now);
 
-var workHours = ["9 am","10 am", "11 am", "12 pm", "13 pm", "14 pm", "15 pm","16 pm", "17 pm"];
+var workHours = [ [9, "am"], [10, "am"], [11, "am"], [12, "pm"], [13, "pm"], [14, "pm"], [15, "pm"], [16, "pm"], [17, "pm"],
+];
 
 for (let i=0 ; i < workHours.length ; i++) {
 
-let time = workHours[i];
+let time = workHours[i][0] + ' ' +  workHours[i][1];
 
 var timeRow = $(
     `<div class="row">
-    <div class="col-2 hour"><h4> ${time}</h4></div>
-    <textarea class="col-8 "></textarea>
-    <button class="col-2 saveBtn">
-        <i class="fas fa-save"></i>
-    </button>
+    <div class="col-2 hour"><h4>${time}</h4></div>
     </div>`
 );
+
+let hour = dayjs().hour();
+
+if (workHours[i][0] < hour) {
+    timeRow.append(
+        `<textarea class="col-8 past"></textarea>
+        <button class="col-2 saveBtn">
+            <i class="fas fa-save"></i>
+        </button>`
+    )
+}
+
+if (workHours[i][0] === hour) {
+    timeRow.append(
+        `<textarea class="col-8 present"></textarea>
+        <button class="col-2 saveBtn">
+            <i class="fas fa-save"></i>
+        </button>`
+    )
+}
+
+if (workHours[i][0] > hour) {
+    timeRow.append(
+        `<textarea class="col-8 future"></textarea>
+        <button class="col-2 saveBtn">
+            <i class="fas fa-save"></i>
+        </button>`
+    )
+}
 
 var timeBlocks = $(".container");
 timeBlocks.append(timeRow);
 
 }
+
