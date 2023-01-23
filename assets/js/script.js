90b267ba07732f43e6f3a28e5098c50709901adc
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-01-23 18:14:22 +0000

    Add basic grid layout for time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,20 @@ var dayName = days[dayjs().day()]
 var now = dayjs().format("D MMMM YYYY");
 
 $("#currentDay").text(dayName + ' ' + now);
+
+var workHours = ["9 am","10 am", "11 am", "12 pm", "13 pm", "14 pm", "15 pm","16 pm", "17 pm"];
+
+for (let i=0 ; i < workHours.length ; i++) {
+
+let time = workHours[i];
+
+var timeRow = $(
+    `<div class="row">
+    <div class="col-2 hour"><h4> ${time}</h4></div>
+    </div>`
+);
+
+var timeBlocks = $(".container");
+timeBlocks.append(timeRow);
+
+}
