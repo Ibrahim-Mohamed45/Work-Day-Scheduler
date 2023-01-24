24aac9a92cbf903c1e18e7c747fceaa6781771ff
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-01-24 12:39:54 +0000

    Style the footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,52 +9,52 @@ var now = dayjs().format("D MMMM YYYY");
 $("#currentDay").text(dayName + ' ' + now);
 
 // Array that contains the typical work hours.
-var workHours = [ [9, "am"], [10, "am"], [11, "am"], [12, "pm"], [13, "pm"], [14, "pm"], [15, "pm"], [16, "pm"], [17, "pm"]];
+var workHours = [[9, "am"], [10, "am"], [11, "am"], [12, "pm"], [13, "pm"], [14, "pm"], [15, "pm"], [16, "pm"], [17, "pm"]];
 
 // For loop to generate a row for each of the work hours (9am till 17pm).
-for (let i=0 ; i < workHours.length ; i++) {
+for (let i = 0; i < workHours.length; i++) {
 
-let time = workHours[i][0] + ' ' +  workHours[i][1];
+    let time = workHours[i][0] + ' ' + workHours[i][1];
 
-var timeRow = $(
-    `<div id="id-${i}" class="row">
+    var timeRow = $(
+        `<div id="id-${i}" class="row">
     <div class="col-2 hour"><h4>${time}</h4></div>
     </div>`
-);
+    );
 
-// Using day.js it gets the hour.
-let hour = dayjs().hour();
+    // Using day.js it gets the hour.
+    let hour = dayjs().hour();
 
-// Changes textarea background color depending if the time is in the  past, present or the future.
-if (workHours[i][0] < hour) {
-    timeRow.append(
-        `<textarea class="col-8 col-md-9 past"></textarea>
+    // Changes textarea background color depending if the time is in the  past, present or the future.
+    if (workHours[i][0] < hour) {
+        timeRow.append(
+            `<textarea class="col-8 col-md-9 past"></textarea>
         <button class="col-2 col-md-1 saveBtn">
             <i class="fas fa-save" ></i>
         </button>`
-    )
-};
+        )
+    };
 
-if (workHours[i][0] === hour) {
-    timeRow.append(
-        `<textarea class="col-8 col-md-9 present"></textarea>
+    if (workHours[i][0] === hour) {
+        timeRow.append(
+            `<textarea class="col-8 col-md-9 present"></textarea>
         <button class="col-2 col-md-1 saveBtn">
             <i class="fas fa-save"></i>
         </button>`
-    )
-};
+        )
+    };
 
-if (workHours[i][0] > hour) {
-    timeRow.append(
-        `<textarea class="col-8 col-md-9 future"></textarea>
+    if (workHours[i][0] > hour) {
+        timeRow.append(
+            `<textarea class="col-8 col-md-9 future"></textarea>
         <button class="col-2 col-md-1 saveBtn">
             <i class="fas fa-save"></i>
         </button>`
-    )
-};
+        )
+    };
 
-var timeBlocks = $(".container");
-timeBlocks.append(timeRow);
+    var timeBlocks = $(".container");
+    timeBlocks.append(timeRow);
 }
 
 // Empty tasks object
@@ -62,31 +62,31 @@ var tasks = {};
 
 // Function to create a tasks object on localStorage and adds the tasks object.
 function storeTasks() {
-  localStorage.setItem("userTasks", JSON.stringify(tasks));
+    localStorage.setItem("userTasks", JSON.stringify(tasks));
 }
 
 // Function to get JSON from localStorage and convert it to an object. 
 function displayStorage() {
-  var storage = JSON.parse(localStorage.getItem("userTasks"));
+    var storage = JSON.parse(localStorage.getItem("userTasks"));
 
-// For loop to update the local object and display it in the textarea.
-  for (var userInput in storage) {
-    tasks[userInput] = storage[userInput];
-    $(`#${userInput}`).children("textarea").text(`${storage[userInput]}`);
-  }
+    // For loop to update the local object and display it in the textarea.
+    for (var userInput in storage) {
+        tasks[userInput] = storage[userInput];
+        $(`#${userInput}`).children("textarea").text(`${storage[userInput]}`);
+    }
 }
 
 displayStorage();
 
 $('.saveBtn').on('click', function () {
-  // Gets the parent id from the row that the save button is clicked. 
-  var rowId = $(this).parent().attr("id");
-  // Gets the text inputted in the row that the save button is clicked.
-  var textareaInput = $(`#${rowId}`).children("textarea").val();
-  // Updates the tasks object with the rowId as the key and textareaInput as a value.
-  tasks[rowId] = textareaInput;
+    // Gets the parent id from the row that the save button is clicked. 
+    var rowId = $(this).parent().attr("id");
+    // Gets the text inputted in the row that the save button is clicked.
+    var textareaInput = $(`#${rowId}`).children("textarea").val();
+    // Updates the tasks object with the rowId as the key and textareaInput as a value.
+    tasks[rowId] = textareaInput;
 
-  storeTasks();
+    storeTasks();
 });
 
 $('#clearBtn').on('click', function () {
