36110d9cd302dfacccbc87e4b5091618a5e74d3e
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-01-24 11:29:57 +0000

    Add on click function that saves tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,14 @@ var now = dayjs().format("D MMMM YYYY");
 
 $("#currentDay").text(dayName + ' ' + now);
 
-var workHours = [ [9, "am"], [10, "am"], [11, "am"], [12, "pm"], [13, "pm"], [14, "pm"], [15, "pm"], [16, "pm"], [17, "pm"],
-];
+var workHours = [ [9, "am"], [10, "am"], [11, "am"], [12, "pm"], [13, "pm"], [14, "pm"], [15, "pm"], [16, "pm"], [17, "pm"]];
 
 for (let i=0 ; i < workHours.length ; i++) {
 
 let time = workHours[i][0] + ' ' +  workHours[i][1];
 
 var timeRow = $(
-    `<div class="row">
+    `<div id="id-${i}" class="row">
     <div class="col-2 hour"><h4>${time}</h4></div>
     </div>`
 );
@@ -22,33 +21,58 @@ let hour = dayjs().hour();
 
 if (workHours[i][0] < hour) {
     timeRow.append(
-        `<textarea class="col-8 past"></textarea>
-        <button class="col-2 saveBtn">
+        `<textarea class="col-9 past"></textarea>
+        <button class="col-1 saveBtn">
             <i class="fas fa-save"></i>
         </button>`
     )
-}
+};
 
 if (workHours[i][0] === hour) {
     timeRow.append(
-        `<textarea class="col-8 present"></textarea>
-        <button class="col-2 saveBtn">
+        `<textarea class="col-9 present"></textarea>
+        <button class="col-1 saveBtn">
             <i class="fas fa-save"></i>
         </button>`
     )
-}
+};
 
 if (workHours[i][0] > hour) {
     timeRow.append(
-        `<textarea class="col-8 future"></textarea>
-        <button class="col-2 saveBtn">
+        `<textarea class="col-9 future"></textarea>
+        <button class="col-1 saveBtn">
             <i class="fas fa-save"></i>
         </button>`
     )
-}
+};
 
 var timeBlocks = $(".container");
 timeBlocks.append(timeRow);
-
 }
+
+var tasks = {};
+
+function storeTasks() {
+  localStorage.setItem("userTasks", JSON.stringify(tasks));
+}
+
+function displayStorage() {
+  var storage = JSON.parse(localStorage.getItem("userTasks"));
+
+  for (var userInput in storage) {
+    tasks[userInput] = storage[userInput];
+    $(`#${userInput}`).children("textarea").text(`${storage[userInput]}`);
+  }
+}
+
+displayStorage();
+
+$('.saveBtn').on('click', function () {
+  var rowId = $(this).parent().attr("id");
+  var textareaInput = $(`#${rowId}`).children("textarea").val();
+
+  tasks[rowId] = textareaInput;
+
+  storeTasks();
+});
 
